3eb8101c37372046b64be5ddde48e05d7b5cc6a8
JianXi <3146624068@qq.com>
2024-01-16 15:48:06 +0800

    第二次修改给下载用户积分加成时会修改密码的BUG

--- src/main/java/com/example/utils/Constants.java
+++ src/main/java/com/example/utils/Constants.java
@@ -2,7 +2,7 @@ package com.example.utils;
 
 public class Constants {
     public final static double pointRate = 0.1;     //给上传者的积分奖励比例
-    public final static Integer uploadPointBonus = 50;           //上传资料给加分
+    public final static Integer uploadPointBonus = 5000;           //上传资料给加分
     public final static String defaultBookcoverUrl = "https://xiao-shu-shu.oss-cn-shanghai.aliyuncs.com/defaultCover.png";           //上传资料给加分
     public final static String defaultBookcoverUuid = "defalutCover";
     public final static String defaultAvatarUrl = "https://xiao-shu-shu.oss-cn-shanghai.aliyuncs.com/defaultAvatar.jpg";           //上传资料给加分
