591da055beadcd7ca97d765d543931f9a5a08cbe
shgurohid <1720931511@qq.com>
2024-01-17 10:20:15 +0800

    优化查询图书

---
+++ src/main/java/com/example/entity/vo/request/user/BookTop.java
@@ -0,0 +1,16 @@
+package com.example.entity.vo.request.user;
+
+import com.example.entity.vo.request.Book;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookTop {
+    private List<Book> books;
+    private int total;//实际获取的图书总数
+
+}
